2ae124da57c4d127a6d5a5ed785c39e8179b6ec6
JWLaessig <s0573711@htw-berlin.de>
2023-05-07 13:31:42 +0200

    26. Commit - das formular für die dateneingabe fehlt noch

--- src/main/java/de/htwberlin/WebTech/WebTechSoSe2023/ui/HelloController.java
+++ src/main/java/de/htwberlin/WebTech/WebTechSoSe2023/ui/HelloController.java
@@ -1,7 +1,14 @@
 package de.htwberlin.WebTech.WebTechSoSe2023.ui;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.htwberlin.WebTech.WebTechSoSe2023.domain.Drink;
+import de.htwberlin.WebTech.WebTechSoSe2023.domain.DrinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class HelloController {
@@ -9,6 +16,19 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @Autowired
+    private DrinkService drinkService;
+
+    @PostMapping("/post")
+    public Drink saveDrink(Drink drink) {
+        return drinkService.save(drink);
+    }
+
+    @GetMapping("/get")
+    public Optional<Drink> getDrink(Long id) {
+        return Optional.ofNullable(drinkService.get(id));
     }
 
 }
